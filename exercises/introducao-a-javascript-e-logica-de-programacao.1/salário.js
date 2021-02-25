631f8e3ee2e44737e72781080aacc13a586dffa0
Huggo Parcelly <h.parcelly@gmail.com>
2021-02-25 18:52:11 -0300

    Exercicio - 11, adiciona o valor do imposto de renda e calcula o salario liquido.

--- exercises/introducao-a-javascript-e-logica-de-programacao.1/salário.js
+++ exercises/introducao-a-javascript-e-logica-de-programacao.1/salário.js
@@ -1,4 +1,4 @@
-let salario = 6000;
+let salario = 3000;
 
 //INSS
 
@@ -18,5 +18,29 @@ if(salario < 0){
 }
 
 // salário com desconto do INSS
-salarioBase = salario - aliquotaInss
+let salarioBase = salario - aliquotaInss
 
+// Imposto de Renda
+
+if (salarioBase <= 1903.98){
+  aliquotaIr = 0;
+  parcelaIr = 0;
+}else if(salarioBase > 1903.98 && salarioBase <= 2826.65){
+  aliquotaIr = 0.075;
+  parcelaIr = 142.80;
+}else if(salarioBase > 2826.65 && salarioBase <= 3751.05){
+  aliquotaIr = 0.15;
+  parcelaIr = 354.80;
+}else if(salarioBase > 3751.05 && salarioBase <= 4664.68){
+  aliquotaIr = 0.225;
+  parcelaIr = 636.13;
+}else if(salarioBase > 4664.68){
+  aliquotaIr = 0.275;
+  parcelaIr = 869.36;
+}
+
+let valorIr = (aliquotaIr * salarioBase) - parcelaIr;
+
+let salarioLiquido = salarioBase - valorIr;
+
+console.log(`O valor do salário a receber é R$ ${salarioLiquido}`)
