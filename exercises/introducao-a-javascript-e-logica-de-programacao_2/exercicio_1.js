ea47e8e873ca8bf950af03756b8c8c2e37340ebc
Huggo Parcelly <h.parcelly@gmail.com>
2021-02-25 19:15:07 -0300

    Exercicio 3 - Adiciona a media dos valores, utilizando o tamanho do array(length).

--- exercises/introducao-a-javascript-e-logica-de-programacao_2/exercicio_1.js
+++ exercises/introducao-a-javascript-e-logica-de-programacao_2/exercicio_1.js
@@ -1,11 +1,22 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
+//Exercicio 1
+
 for(let i = 0; i < numbers.length; i += 1){
   console.log(numbers[i]);
 }
+
+//Exercicio 2
 
 let soma = 0;
 for(let i = 0; i < numbers.length; i += 1){
   soma += numbers[i];
 }
 console.log(`A soma dos  valores é: ${soma}`);
+
+//Exercicio 3
+
+let media=0;
+
+media = soma / numbers.length;
+console.log(`A média dos valores é: ${media}`);
