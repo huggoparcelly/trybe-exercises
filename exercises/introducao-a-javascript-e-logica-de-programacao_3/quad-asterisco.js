e91a9a8f5a38dfcf0b42439e8dc2d50b5488c545
Huggo Parcelly <h.parcelly@gmail.com>
2021-02-26 18:57:04 -0300

    Exercicio - 1, criaçao de quadrado com asteriscos

---
+++ exercises/introducao-a-javascript-e-logica-de-programacao_3/quad-asterisco.js
@@ -0,0 +1,11 @@
+let n = 10;
+let asterisco = "*";
+let saida = ""
+
+for (let linha = 0; linha < n; linha += 1){
+  saida += asterisco
+}
+
+for (let linha = 0; linha < n; linha += 1){
+  console.log(saida);
+}
