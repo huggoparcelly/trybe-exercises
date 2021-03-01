3fa4c1975ac1721499ea10a2c1ebfe1a6e0b38e6
Huggo Parcelly <h.parcelly@gmail.com>
2021-03-01 19:36:46 -0300

    Exercici 2 - Funcao que verifica qual eh o indice do maior numero de uma array

---
+++ exercises/introducao-a-javascript-e-logica-de-programacao_4/indice-maior.js
@@ -0,0 +1,16 @@
+function bigIndex(array) {
+  let maior = array[0];
+  let indexBig = 0;
+  
+  for(index in array){
+    if(maior < array[index]){
+      maior = array[index];
+      indexBig = index;
+    }
+  }
+  return indexBig;
+}
+
+arrayTeste = [2, 4, 6, 7, 10, 0, -3];
+
+console.log(bigIndex(arrayTeste));
