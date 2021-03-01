f24729a8b5bbac956b92b6a25e9e4ec2f4c913fb
Huggo Parcelly <h.parcelly@gmail.com>
2021-03-01 16:09:33 -0300

    Adiciona exercicios triangulos

--- exercises/introducao-a-javascript-e-logica-de-programacao_3/quad-asterisco.js
+++ exercises/introducao-a-javascript-e-logica-de-programacao_3/quad-asterisco.js
@@ -1,11 +1,11 @@
 let n = 10;
 let asterisco = "*";
-let saida = ""
+let saida = "";
 
 for (let linha = 0; linha < n; linha += 1){
-  saida += asterisco
+  saida += asterisco;
 }
 
-for (let linha = 0; linha < n; linha += 1){
+for (let sai = 0; sai < n; sai += 1){
   console.log(saida);
 }
--- exercises/introducao-a-javascript-e-logica-de-programacao_3/trian-esq-asteriscos.js
+++ exercises/introducao-a-javascript-e-logica-de-programacao_3/trian-esq-asteriscos.js
@@ -2,7 +2,7 @@ let n = 5;
 let asterisco = "*";
 let saida = ""
 
-for (let linha = 0; linha < n; linha += 1){
+for (let linha = 0; linha <= n; linha += 1){
   saida += asterisco;
   console.log(saida);
 }
