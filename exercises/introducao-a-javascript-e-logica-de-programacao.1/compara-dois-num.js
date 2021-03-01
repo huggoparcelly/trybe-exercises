6c72d5f48553d787a23d9cbebf56b772ba98ef1a
Huggo Parcelly <h.parcelly@gmail.com>
2021-03-01 16:15:40 -0300

    Adiciona funcao para comparar dois numeros

--- exercises/introducao-a-javascript-e-logica-de-programacao.1/compara-dois-num.js
+++ exercises/introducao-a-javascript-e-logica-de-programacao.1/compara-dois-num.js
@@ -6,3 +6,16 @@ if (num1 > num2){
 }else {
   console.log("O segundo é o maior");
 }
+
+
+// function
+
+function compareTwo (num1, num2) {
+  if (num1 > num2){
+    console.log("o primeiro é o maior");
+  }else {
+    console.log("O segundo é o maior");
+  }
+};
+
+compareTwo(200 , 20);
