3cf465dab5d809e65b8ff434db9898442241ef40
Huggo Parcelly <h.parcelly@gmail.com>
2021-03-01 18:57:03 -0300

    Adiciona um for que junta as informaçoes nos objetos

--- exercises/introducao-a-javascript-e-logica-de-programacao_4/objetos-for-in.js
+++ exercises/introducao-a-javascript-e-logica-de-programacao_4/objetos-for-in.js
@@ -2,11 +2,18 @@ let info = {
   personagem: "Margarida",
   origem: "Pato Donald",
   nota: "Namorada do personagem principal nos quadrinhos do Pato Donald",
+  recorrente: 'Sim'
 };
 
-console.log('Bem-vinda, ' + info.personagem);
+let infoTio = {
+  personagem: "Tio Patinhas",
+  origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
+  nota: "O último MacPatinhas",
+  recorrente: 'Sim'
+};
 
-info.recorrente = 'Sim'
+
+console.log('Bem-vinda, ' + info.personagem);
 
 console.log(info)
 
@@ -16,4 +23,14 @@ for (key in info){
 
 for (key in info){
   console.log(info[key]);
+};
+
+
+
+for (key in info){
+  if (info[key] === info.recorrente && info[key] === 'Sim' && infoTio[key] === 'Sim'){
+    console.log('Ambos recorrentes');
+  } else{
+    console.log(info[key] + ' e ' + infoTio[key]);
+  }
 };
