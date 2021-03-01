c8e0be64beaddd0fb5c6c83c2bcfbf03ecc3a665
Huggo Parcelly <h.parcelly@gmail.com>
2021-03-01 16:17:20 -0300

    Adiciona funcao para comprar tres numeros

--- exercises/introducao-a-javascript-e-logica-de-programacao.1/compara-dois-num.js
+++ exercises/introducao-a-javascript-e-logica-de-programacao.1/compara-dois-num.js
@@ -1,13 +1,3 @@
-let num1 = 100;
-let num2 = 13;
-
-if (num1 > num2){
-  console.log("o primeiro é o maior");
-}else {
-  console.log("O segundo é o maior");
-}
-
-
 // function
 
 function compareTwo (num1, num2) {
