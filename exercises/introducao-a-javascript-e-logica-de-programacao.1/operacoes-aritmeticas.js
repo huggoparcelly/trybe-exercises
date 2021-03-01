822e063a7bbc699375973bd8bfc07be325607bf3
Huggo Parcelly <h.parcelly@gmail.com>
2021-03-01 16:30:06 -0300

    Adiciona funcao que retorna as operacoes aritmeticas

--- exercises/introducao-a-javascript-e-logica-de-programacao.1/operacoes-aritmeticas.js
+++ exercises/introducao-a-javascript-e-logica-de-programacao.1/operacoes-aritmeticas.js
@@ -1,9 +1,12 @@
-let a = 3;
-let b = 8;
+// function
 
+function operations(a, b){
+  console.log("Soma:", a+b);
+  console.log("Subtração:", a-b);
+  console.log("Multiplicação", a*b);
+  console.log("Divisão", a/b);
+  console.log("Módulo:", a%b)
+};
 
-console.log("Soma:", a+b);
-console.log("Subtração:", a-b);
-console.log("Multiplicação", a*b);
-console.log("Divisão", a/b);
-console.log("Módulo:", a%b)
+operations(2,4);
+
