3800665040c0cb6b4bbf8e258ad26099629251ab
Huggo Parcelly <h.parcelly@gmail.com>
2021-03-03 19:33:17 -0300

    Altera o texto padrao

---
+++ exercises/javascript-dom-eventos-e-web-storage_1/script.js
@@ -0,0 +1,7 @@
+function newText(text) {
+  let oldText = document.getElementsByTagName('p');
+  oldText[0].innerHTML = text;
+}
+
+newText('Realizado profissionalmente, feliz, bem fisica e mentalmente.')
+
