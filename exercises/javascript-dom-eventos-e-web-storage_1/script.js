4ec30175bd1dc761a93f4fa29793d972fc261f48
Huggo Parcelly <h.parcelly@gmail.com>
2021-03-04 10:28:41 -0300

    Adiciona o background color verde

--- exercises/javascript-dom-eventos-e-web-storage_1/script.js
+++ exercises/javascript-dom-eventos-e-web-storage_1/script.js
@@ -1,7 +1,11 @@
 function newText(text) {
-  let oldText = document.getElementsByTagName('p');
-  oldText[0].innerHTML = text;
+  document.getElementsByTagName('p')[0].innerHTML = text;
 }
 
 newText('Realizado profissionalmente, feliz, bem fisica e mentalmente.')
 
+function newBgColor(bgColor) {
+  document.getElementsByClassName('main-content')[0].style.backgroundColor = bgColor;
+}
+
+newBgColor('rgb(76,164,109)')
