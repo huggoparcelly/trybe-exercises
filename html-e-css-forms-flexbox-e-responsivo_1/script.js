c0d902e5975df3a6e9b5588004b0b729f3317bde
Huggo Parcelly <h.parcelly@gmail.com>
2021-03-16 18:23:38 -0300

    atualizacao do exercicio

--- html-e-css-forms-flexbox-e-responsivo_1/script.js
+++ html-e-css-forms-flexbox-e-responsivo_1/script.js
@@ -1,12 +1,9 @@
 const estadosBr = ['Acre','Alagoas', 'Amapá', 'Amazonas', 'Bahia', 'Ceará', 'Distrito Federal', 'Espírito Santo', 'Goiás', 'Maranhão', 'Mato Grosso', 'Mato Grosso do Sul', 'Minas Gerais', 'Pará', 'Paraíba', 'Paraná', 'Pernambuco', 'Piauí', 'Rio de Janeiro', 'Rio Grande do Norte', 'Rio Grande do Sul', 'Rondônia', 'Roraima', 'Santa Catarina', 'São Paulo', 'Sergipe', 'Tocantins'];
 
-console.log(estadosBr[0])
-console.log(estadosBr.length);
-
-const dropDownList = document.querySelector('#input-estado');
+const dropDownList = document.querySelector('#input-estado'); // captura o drop Down
 
 for (let i = 0; i < estadosBr.length; i+= 1) {
-  const createStateList = document.createElement('option');
-  createStateList.innerText = estadosBr[i];
-  dropDownList.appendChild(createStateList);
+  const createStateList = document.createElement('option'); // cria uma option
+  createStateList.innerText = estadosBr[i]; // a option recebe o texto contido na lista de estados
+  dropDownList.appendChild(createStateList); // coloca a option dentro do dropdown
 }
