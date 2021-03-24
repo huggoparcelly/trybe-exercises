1cacd2a1899bcd260e9defd782fbd75b840a72f1
Huggo Parcelly <h.parcelly@gmail.com>
2021-03-24 18:06:33 -0300

    Adiciona o gabarito da questao4

--- exercises/introducao-a-javascript-es6-e-testes-unitarios_1/exercicio3-p2.js
+++ exercises/introducao-a-javascript-es6-e-testes-unitarios_1/exercicio3-p2.js
@@ -1,7 +1,4 @@
 let clickCount = 0
 const buttonCount = document.getElementById('btn-cont');
 const count = document.getElementById('contagem');
-buttonCount.addEventListener('click', () => {
-  clickCount += 1
-  count.innerHTML = clickCount;
-});
+buttonCount.addEventListener('click', () => count.innerHTML = clickCount += 1);
