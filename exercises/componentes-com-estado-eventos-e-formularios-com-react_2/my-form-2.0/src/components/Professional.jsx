9b95ecf086a502f92e352d9011eab8dad46b8eb3
Huggo Parcelly <h.parcelly@gmail.com>
2021-06-05 15:25:37 -0300

    adiciona o componente Professional completo

--- exercises/componentes-com-estado-eventos-e-formularios-com-react_2/my-form-2.0/src/components/Professional.jsx
+++ exercises/componentes-com-estado-eventos-e-formularios-com-react_2/my-form-2.0/src/components/Professional.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { actionInfos } from '../redux/actions';
 
 class Professional extends React.Component {
   render () {
+    const { update } = this.props;
     return (
       <>
         <h3>Informações Profissional</h3>
@@ -11,24 +14,31 @@ class Professional extends React.Component {
             id="curriculum"
             placeholder="Informações relevantes"
             maxLength="1000"
+            onChange = {(e) => update(e.target.id, e.target.value)}
           />
           <label htmlFor="officer">Cargo</label>
           <input
             id="officer"
             type="text"
             placeholder="Cargo"
+            onChange = {(e) => update(e.target.id, e.target.value)}
           />
           <label htmlFor="description">Descrição</label>
           <textarea 
             id="description"
             placeholder="Descrição do cargo"
             maxLength="500"
+            onChange = {(e) => update(e.target.id, e.target.value)}
           />
         </form>
-        
+
       </>
     )
   }
 }
 
-export default Professional;
+const mapDispatchToprops = (dispatch) => ({
+  update: (type, value) => dispatch(actionInfos(type, value)),
+});
+
+export default connect(null, mapDispatchToprops)(Professional);
--- exercises/componentes-com-estado-eventos-e-formularios-com-react_2/my-form-2.0/src/redux/reducers/professionalReducer.js
+++ exercises/componentes-com-estado-eventos-e-formularios-com-react_2/my-form-2.0/src/redux/reducers/professionalReducer.js
@@ -1,7 +1,29 @@
-const INITIAL_STATE = []
+const INITIAL_STATE = {
+    curriculum: '',
+		officer: '',
+		description: '',
+}
 
 function professionalReducer(state = INITIAL_STATE, action) {
-  return state;
+  switch (action.type) {
+    case 'CURRICULUM':
+      return {
+        ...state,
+        curriculum: action.value,
+      };
+    case 'OFFICER':
+      return {
+        ...state,
+        curriculum: action.value,
+      };
+    case 'DESCRIPTION':
+      return {
+        ...state,
+        description: action.value,
+      };
+    default:
+      return state;
+  }
 }
 
 export default professionalReducer;
