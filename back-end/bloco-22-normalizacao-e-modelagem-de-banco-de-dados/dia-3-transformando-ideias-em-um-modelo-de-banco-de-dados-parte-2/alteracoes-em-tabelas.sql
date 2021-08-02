4be9cf3330e947d45aa3159f365a12353bb2869d
Huggo Parcelly <h.parcelly@gmail.com>
2021-08-02 19:16:34 -0300

    alteracoes em tabelas

---
+++ back-end/bloco-22-normalizacao-e-modelagem-de-banco-de-dados/dia-3-transformando-ideias-em-um-modelo-de-banco-de-dados-parte-2/alteracoes-em-tabelas.sql
@@ -0,0 +1,6 @@
+ALTER TABLE hr.locations CHANGE street_address address VARCHAR(40);
+
+ALTER TABLE hr.regions CHANGE region_name region VARCHAR(25);
+
+ALTER TABLE hr.countries CHANGE country_name country VARCHAR(40);
+
