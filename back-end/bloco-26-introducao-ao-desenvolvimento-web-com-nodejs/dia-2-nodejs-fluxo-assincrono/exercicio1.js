5708d96827b6c103344b50f9faab3d5bcae34162
Huggo Parcelly <h.parcelly@gmail.com>
2021-08-25 18:49:00 -0300

    funcao que retorna uma promise

---
+++ back-end/bloco-26-introducao-ao-desenvolvimento-web-com-nodejs/dia-2-nodejs-fluxo-assincrono/exercicio1.js
@@ -0,0 +1,14 @@
+function somaMult(a, b, c) {
+  return new Promise((resolve, reject) => {
+    if (isNaN(a) || isNaN(b) || isNaN(c)) reject( new Error("Informe apenas números"));
+
+    const result = (a + b) * c;
+    if(result < 50 ) reject(new Error("Valor muito baixo"));
+    
+    resolve(result);
+  })
+}
+
+somaMult(10, 20, 40)
+  .then((result) => console.log(result))
+  .catch((err) => console.log(err.message))
