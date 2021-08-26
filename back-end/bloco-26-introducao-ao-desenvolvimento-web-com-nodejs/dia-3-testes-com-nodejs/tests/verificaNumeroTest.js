b36680d2c422a9195b2c98988af1918ae8414700
Huggo Parcelly <h.parcelly@gmail.com>
2021-08-26 19:15:07 -0300

    adiciona o tratamento para quando nao for um numero

--- back-end/bloco-26-introducao-ao-desenvolvimento-web-com-nodejs/dia-3-testes-com-nodejs/tests/verificaNumeroTest.js
+++ back-end/bloco-26-introducao-ao-desenvolvimento-web-com-nodejs/dia-3-testes-com-nodejs/tests/verificaNumeroTest.js
@@ -2,30 +2,41 @@ const {expect} = require('chai');
 const verificaNumero = require('../verificaNumero');
 
 describe('Executa o script que verifica numeros', () => {
-  describe('a resposta', () => {
-    
-    it('é uma "string"', () => {
-      const resposta = verificaNumero(1);
-
-      expect(resposta).to.be.a('string');
+  describe('Quando o parâmetro for um número', () => {
+    describe('a resposta é', () => {
+      
+      it('uma "string"', () => {
+        const resposta = verificaNumero(1);
+  
+        expect(resposta).to.be.a('string');
+      });
+  
+      it('"positivo" para numero maior que zero', () => {
+        const resposta = verificaNumero(1);
+  
+        expect(resposta).to.be.equal('positivo')
+      });
+  
+      it('"negativo" para numero menor que zero', () => {
+        const resposta = verificaNumero(-1);
+  
+        expect(resposta).to.be.equal('negativo');
+      });
+  
+      it('"neutro" quando for igual a zero', () => {
+        const resposta = verificaNumero(0);
+  
+        expect(resposta).to.be.equal('neutro');
+      })
     });
+  });
+  describe('Quando o parâmetro não for um número', () => {
+    describe('a resposta é', () => {
+      it('o valor deve ser um número', () => {
+        const resposta = verificaNumero('a');
 
-    it('positivo para numero maior que zero', () => {
-      const resposta = verificaNumero(1);
-
-      expect(resposta).to.be.equal('positivo')
+        expect(resposta).to.be.equal('o valor deve ser um número');
+      })
     });
-
-    it('negativo para numero menor que zero', () => {
-      const resposta = verificaNumero(-1);
-
-      expect(resposta).to.be.equal('negativo');
-    });
-
-    it('neutro quando for igual a zero', () => {
-      const resposta = verificaNumero(0);
-
-      expect(resposta).to.be.equal('neutro');
-    })
   })
 })
--- back-end/bloco-26-introducao-ao-desenvolvimento-web-com-nodejs/dia-3-testes-com-nodejs/verificaNumero.js
+++ back-end/bloco-26-introducao-ao-desenvolvimento-web-com-nodejs/dia-3-testes-com-nodejs/verificaNumero.js
@@ -1,4 +1,5 @@
 function verificaNumero(num) {
+  if(isNaN(num)) return "o valor deve ser um número"
   if(num>0) return "positivo";
   else if(num<0) return "negativo";
   return "neutro";
