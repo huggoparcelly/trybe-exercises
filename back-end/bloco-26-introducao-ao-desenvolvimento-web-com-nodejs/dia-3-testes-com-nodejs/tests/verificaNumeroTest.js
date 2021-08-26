b733b0795c28c033eef314e474eeda79dfe4df79
Huggo Parcelly <h.parcelly@gmail.com>
2021-08-26 19:01:08 -0300

    cria a funçao que verifica o numero

--- back-end/bloco-26-introducao-ao-desenvolvimento-web-com-nodejs/dia-3-testes-com-nodejs/tests/verificaNumeroTest.js
+++ back-end/bloco-26-introducao-ao-desenvolvimento-web-com-nodejs/dia-3-testes-com-nodejs/tests/verificaNumeroTest.js
@@ -5,25 +5,25 @@ describe('Executa o script que verifica numeros', () => {
   describe('a resposta', () => {
     
     it('é uma "string"', () => {
-      const resposta = verificaNumero();
+      const resposta = verificaNumero(1);
 
       expect(resposta).to.be.a('string');
     });
 
     it('positivo para numero maior que zero', () => {
-      const resposta = verificaNumero();
+      const resposta = verificaNumero(1);
 
       expect(resposta).to.be.equal('positivo')
     });
 
     it('negativo para numero menor que zero', () => {
-      const resposta = verificaNumero();
+      const resposta = verificaNumero(-1);
 
       expect(resposta).to.be.equal('negativo');
     });
 
     it('neutro quando for igual a zero', () => {
-      const resposta = verificaNumero();
+      const resposta = verificaNumero(0);
 
       expect(resposta).to.be.equal('neutro');
     })
--- back-end/bloco-26-introducao-ao-desenvolvimento-web-com-nodejs/dia-3-testes-com-nodejs/verificaNumero.js
+++ back-end/bloco-26-introducao-ao-desenvolvimento-web-com-nodejs/dia-3-testes-com-nodejs/verificaNumero.js
@@ -1,5 +1,7 @@
 function verificaNumero(num) {
-  
+  if(num>0) return "positivo";
+  else if(num<0) return "negativo";
+  return "neutro";
 }
 
 module.exports = verificaNumero;
