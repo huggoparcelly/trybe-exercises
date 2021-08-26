bc446b94f0845eb1674da1f414ab5b2f2c2b2710
Huggo Parcelly <h.parcelly@gmail.com>
2021-08-26 19:24:06 -0300

    separando os testes em casos

--- back-end/bloco-26-introducao-ao-desenvolvimento-web-com-nodejs/dia-3-testes-com-nodejs/tests/verificaNumeroTest.js
+++ back-end/bloco-26-introducao-ao-desenvolvimento-web-com-nodejs/dia-3-testes-com-nodejs/tests/verificaNumeroTest.js
@@ -2,7 +2,8 @@ const {expect} = require('chai');
 const verificaNumero = require('../verificaNumero');
 
 describe('Executa o script que verifica numeros', () => {
-  describe('Quando o parâmetro for um número', () => {
+  
+  describe('Quando o parâmetro for um número maior que zero', () => {
     describe('a resposta é', () => {
       
       it('uma "string"', () => {
@@ -10,26 +11,47 @@ describe('Executa o script que verifica numeros', () => {
   
         expect(resposta).to.be.a('string');
       });
-  
-      it('"positivo" para numero maior que zero', () => {
+
+      it('positivo', () => {
         const resposta = verificaNumero(1);
   
         expect(resposta).to.be.equal('positivo')
       });
+    })
+  });
   
-      it('"negativo" para numero menor que zero', () => {
+  describe('Quando o parâmetro for um número menor que zero', () => {
+    describe('a resposta é', () => {
+      it('uma "string"', () => {
+        const resposta = verificaNumero(1);
+  
+        expect(resposta).to.be.a('string');
+      });
+
+      it('negativo', () => {
         const resposta = verificaNumero(-1);
   
         expect(resposta).to.be.equal('negativo');
       });
+    });
+  });
   
-      it('"neutro" quando for igual a zero', () => {
+  describe('Quando o parâmetro for zero', () => {
+    describe('a resposta é', () => {
+      it('uma "string"', () => {
+        const resposta = verificaNumero(1);
+  
+        expect(resposta).to.be.a('string');
+      });
+      
+      it('neutro', () => {
         const resposta = verificaNumero(0);
   
         expect(resposta).to.be.equal('neutro');
-      })
+      });
     });
   });
+
   describe('Quando o parâmetro não for um número', () => {
     describe('a resposta é', () => {
       it('o valor deve ser um número', () => {
