38f929e08dc15310167b2c9acf8f0bfee3ba08e5
Huggo Parcelly <h.parcelly@gmail.com>
2021-08-29 20:17:34 -0300

    cria uma rota post para hello usuario

--- back-end/bloco-26-introducao-ao-desenvolvimento-web-com-nodejs/dia-4-express-http-com-nodejs/index.js
+++ back-end/bloco-26-introducao-ao-desenvolvimento-web-com-nodejs/dia-4-express-http-com-nodejs/index.js
@@ -1,9 +1,16 @@
+const bodyParser = require('body-parser');
 const express = require('express');
 
 const app = express();
+app.use(bodyParser.json());
 
 app.get('/ping', (req, res) => {
-  res.status(200).json({message: 'pong'});
+  return res.status(200).json({message: 'pong'});
+});
+
+app.post('/hello', (req, res) => {
+  const {name} = req.body;
+  res.status(201).json({message: `Hello, ${name}`});
 });
 
 app.listen(3000);
