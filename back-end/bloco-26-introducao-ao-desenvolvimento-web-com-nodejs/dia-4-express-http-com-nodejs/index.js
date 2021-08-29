1a58d8a2ea0cdc2cb221777ad135c1d450b66c59
Huggo Parcelly <h.parcelly@gmail.com>
2021-08-29 20:32:32 -0300

    cria uma rota put que utiliza o nome e a idade pelo body

--- back-end/bloco-26-introducao-ao-desenvolvimento-web-com-nodejs/dia-4-express-http-com-nodejs/index.js
+++ back-end/bloco-26-introducao-ao-desenvolvimento-web-com-nodejs/dia-4-express-http-com-nodejs/index.js
@@ -17,7 +17,12 @@ app.post('/greetings', (req, res) => {
   const {name, age} = req.body;
   if(age <= 17) return res.status(401).json({message: "Unauthorized"})
   return res.status(200).json({message: `Hello, ${name}`});
+});
 
+
+app.put('/users/:name/:age', (req, res) => {
+  const {name, age} = req.body;
+  return res.status(200).json({message: `Seu nome é ${name} e você tem ${age} anos de idade`})
 });
 
 app.listen(3000);
