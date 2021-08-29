d399dcb536aff94925d1bbb5c17761d5395bd079
Huggo Parcelly <h.parcelly@gmail.com>
2021-08-29 20:48:06 -0300

    cria as funçoes de leitura do json e o metodo get para retornar o arquivo

--- back-end/bloco-26-introducao-ao-desenvolvimento-web-com-nodejs/dia-4-express-http-com-nodejs/index.js
+++ back-end/bloco-26-introducao-ao-desenvolvimento-web-com-nodejs/dia-4-express-http-com-nodejs/index.js
@@ -1,6 +1,8 @@
 const bodyParser = require('body-parser');
 const express = require('express');
 
+const simpsonsUtils = require('./fs-utils');
+
 const app = express();
 app.use(bodyParser.json());
 
@@ -24,5 +26,12 @@ app.put('/users/:name/:age', (req, res) => {
   const {name, age} = req.body;
   return res.status(200).json({message: `Seu nome é ${name} e você tem ${age} anos de idade`})
 });
+
+app.get('/simpsons', async (req, res) => {
+  const simpsons = await simpsonsUtils.getSimpsons();
+
+  res.status(200).json(simpsons);
+});
+
 
 app.listen(3000);
