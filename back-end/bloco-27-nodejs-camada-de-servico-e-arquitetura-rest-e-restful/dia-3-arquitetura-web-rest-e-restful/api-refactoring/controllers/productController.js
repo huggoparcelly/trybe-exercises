57091133bcd68bd5500ef1c2740cd0ef1312bb74
Huggo Parcelly <h.parcelly@gmail.com>
2021-09-08 18:54:19 -0300

    refatora os endpoints, verbos e respostas no CRUD

--- back-end/bloco-27-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-3-arquitetura-web-rest-e-restful/api-refactoring/controllers/productController.js
+++ back-end/bloco-27-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-3-arquitetura-web-rest-e-restful/api-refactoring/controllers/productController.js
@@ -1,40 +1,41 @@
 const express = require('express');
+const { StatusCodes } = require('http-status-codes');
 const ProductModel = require('../models/productModel');
 
 const router = express.Router();
 
-router.get('/list-products', async (req, res, next) => {
+router.get('/', async (req, res, next) => {
   const products = await ProductModel.getAll();
 
-  res.send(products);
+  res.status(StatusCodes.OK).json(products);
 });
 
-router.get('/get-by-id/:id', async (req, res, next) => {
+router.get('/:id', async (req, res, next) => {
   const product = await ProductModel.getById(req.params.id);
 
-  res.send(product);
+  res.status(StatusCodes.OK).json(product);
 });
 
-router.post('/add-user', async (req, res) => {
+router.post('/', async (req, res) => {
   const { name, brand } = req.body;
 
   const newProduct = await ProductModel.add(name, brand);
 
-  res.send(newProduct);
+  res.status(StatusCodes.CREATED).json(newProduct);
 });
 
-router.post('/delete-user/:id', async (req, res) => {
+router.delete('/:id', async (req, res) => {
   const products = await ProductModel.exclude(req.params.id);
 
-  res.send(products);
+  res.status(StatusCodes.NO_CONTENT).json();
 });
 
-router.post('/update-user/:id', async (req, res) => {
+router.put('/:id', async (req, res) => {
   const { name, brand } = req.body;
 
   const products = await ProductModel.update(req.params.id, name, brand);
 
-  res.send(products);
+  res.status(StatusCodes.OK).json(products);
 });
 
 module.exports = router;
