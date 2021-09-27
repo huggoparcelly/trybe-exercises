85350e3e09d4f758ed8df4b056f9c2cd00a3c5e8
Huggo Parcelly <h.parcelly@gmail.com>
2021-09-27 19:44:59 -0300

    adiciona exercicios do dia 28.01

--- back-end/bloco-29-deployment/dia-1-infraestrutura-deploy-com-heroku/api-heroku-deploy/indexProcfile.js
+++ back-end/bloco-29-deployment/dia-1-infraestrutura-deploy-com-heroku/api-heroku-deploy/indexProcfile.js
@@ -5,7 +5,7 @@ const app = express();
 const SERVER_API = process.env.SERVER_API || 'Não foi possível!'
 
 app.get('/', (req, res) => {
-  res.send(`Está no ambiente de ${SERVER_API}`)
+  res.send(`Procfile funciona mesmo!`)
 })
 
 const PORT = process.env.PORT || 3000;
