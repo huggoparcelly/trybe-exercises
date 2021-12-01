a57ce7b1477c4db51fee7c9627fcf71ccb2c8099
Huggo Parcelly <h.parcelly@gmail.com>
2021-11-30 22:01:27 -0300

    adiciona as palaavras do arquivo em uma lista como parametro da funcao do jogo

--- ciencia-da-computacao/bloco-32-introducao-a-python/dia-2-entrada-e-saida-de-dados/palavra_embaralhada.py
+++ ciencia-da-computacao/bloco-32-introducao-a-python/dia-2-entrada-e-saida-de-dados/palavra_embaralhada.py
@@ -1,20 +1,19 @@
 import random
 
-WORDS = ["cortador", "origame", "papel"]
-
 
 def scrambled_game(words):
-    random_word = random.choice(WORDS)
+    random_word = random.choice(words)
     scrambled_word = "".join(random.sample(random_word, len(random_word)))
 
     print(scrambled_word)
     player_word = input("Qual é a palavra? ")
 
-    if player_word.lower() in WORDS:
+    if player_word.lower() in words:
         print("Parabéns, você acertou!")
     else:
         print("Poxa, não foi dessa vez...")
 
 
 if __name__ == "__main__":
+    WORDS = ["cortador", "origame", "papel"]
     scrambled_game(WORDS)
--- ciencia-da-computacao/bloco-32-introducao-a-python/dia-2-entrada-e-saida-de-dados/palavras_embaralhadas_arquivo.py
+++ ciencia-da-computacao/bloco-32-introducao-a-python/dia-2-entrada-e-saida-de-dados/palavras_embaralhadas_arquivo.py
@@ -2,4 +2,7 @@ from palavra_embaralhada import scrambled_game
 
 
 with open("palavras.txt", mode="r") as file:
-    scrambled_game(file)
+    lista = []
+    for word in file:
+        lista.append(word.replace('\n', ''))
+    scrambled_game(lista)
