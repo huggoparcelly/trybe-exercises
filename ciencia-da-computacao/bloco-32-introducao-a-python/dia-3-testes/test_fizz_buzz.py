4c1a42f280f69931711c8764815acf4052611aec
Huggo Parcelly <h.parcelly@gmail.com>
2021-12-01 18:27:19 -0300

    adiciona os testes para os possiveis retornos da funcao fizz_buzz

--- ciencia-da-computacao/bloco-32-introducao-a-python/dia-3-testes/test_fizz_buzz.py
+++ ciencia-da-computacao/bloco-32-introducao-a-python/dia-3-testes/test_fizz_buzz.py
@@ -8,8 +8,21 @@ def test_when_n_is_not_a_number():
         fizz_buzz(n)
 
 
-# def test_when_n_is_divisible_of_three():
+def test_when_n_is_not_divisible_for_three_or_five():
+    n = 2
+    assert fizz_buzz(n) == [1, 2]
 
-# def test_when_n_is_divisible_of_five():
 
-# def test_when_n_is_divisible_of_three_and_five():
+def test_when_n_is_divisible_for_three():
+    n = 3
+    assert "Fizz" in fizz_buzz(n)
+
+
+def test_when_n_is_divisible_for_five():
+    n = 5
+    assert "Buzz" in fizz_buzz(n)
+
+
+def test_when_n_is_divisible_for_three_and_five():
+    n = 15
+    assert "FizzBuzz" in fizz_buzz(n)
